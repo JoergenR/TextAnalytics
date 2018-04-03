77e971b1e0e912eef5de4089f50be4f87abb4e77
JoergenR <jorgen.ringstad@belronsolutions.no>
2018-04-03 21:46:32 +0200

    Working on it
    Working on it

---
+++ src/binarySearchTree/binarySearchTree.java
@@ -0,0 +1,112 @@
+package binarySearchTree;
+
+// Standard operations on binary search trees
+////
+//// Nodes contain just a single character in addition to the two
+//// pointers to left and right child
+//
+
+public class binarySearchTree
+{
+    // Inner class for binary tree nodes with single-character data
+    private class treeNode
+    {
+        String data;
+        treeNode left, right;
+
+        public treeNode(String value)
+        {
+            data = value;
+            left = right = null;
+        }
+
+        void write()
+        {
+            System.out.print(data + " ");
+        }
+    }
+
+    // Root of entire search tree
+    private treeNode root;
+
+    // Constructor, creates empty tree
+    public void binarySearchTree()
+    {
+        root = null;
+    }
+
+    // Check for empty tree
+    public boolean isEmpty()
+    {
+        return (root == null);
+    }
+
+
+    //
+    // Insertion of new value into binary search tree
+    // Trenger en variabel for å telle antall
+    public void insert(String value)
+    {
+        treeNode newNode = new treeNode(value);
+        int num = 1;
+
+        // Create new root if tree is empty
+        if (isEmpty())
+        {
+            root = newNode;
+            return;
+        }
+
+        treeNode current = root;
+        boolean finished = false;
+
+        // Insert new node as a leaf in the tree
+        while (!finished)
+        {
+            //compareTo
+            if (value.compareTo(current.data) == 0 )
+            {
+                if (current.left == null)
+                {
+                    current.left = newNode;
+                    finished = true;
+                }
+                else
+                    current = current.left;
+            }
+            else
+            {
+                if (current.right == null)
+                {
+                    current.right = newNode;
+                    finished = true;
+                }
+                else
+                    current = current.right;
+            }
+        }
+    }
+
+
+
+
+    //
+    // Printout of inorder tree traversal, for demo purposes
+    //
+    public void inorder()
+    {
+        System.out.print("Inorder: ");
+        inorder(root);
+        System.out.println("\n");
+    }
+
+    private void inorder(treeNode t)
+    {
+        if (t != null)
+        {
+            inorder(t.left);
+            t.write();
+            inorder(t.right);
+        }
+    }
+}
---
+++ src/main/Main.java
@@ -0,0 +1,36 @@
+package main;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.StreamTokenizer;
+import binarySearchTree.binarySearchTree;
+
+public class Main {
+
+    
+    public static void main(String[] args)
+    {
+        String in = null;
+        BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
+        StreamTokenizer input = new StreamTokenizer(r);
+        binarySearchTree bst = new binarySearchTree();
+
+
+        System.out.println("Enter text: ");
+        try
+        {
+            int x = input.nextToken();
+            while (x != input.TT_EOF)
+            {
+                if (input.ttype == input.TT_WORD)
+                  in = input.sval.toUpperCase().replaceAll("[^a-zA-Z0-9]"," ");
+                x = input.nextToken ();
+
+
+            }
+            bst.insert(in);
+        }
+        catch (IOException e) {};
+    }
+}
